16bf77abc5c457eef0c84757c61a7883b5ffe613
Peter Tinti <volgorean@gmail.com>
2014-10-25 05:22:03 -0700

    consolidated resource serving

--- main.go
+++ main.go
@@ -6,11 +6,10 @@ import (
 
 func main() {
 	http.HandleFunc("/", rootController)
-	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("css"))))
-	http.Handle("/js/", http.StripPrefix("/js/", http.FileServer(http.Dir("js"))))
+	http.Handle("/resources/", http.StripPrefix("/resources/", http.FileServer(http.Dir("resources"))))
 	http.ListenAndServe(":8080", nil)
 }
 
 func rootController(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "index.html")
+	http.ServeFile(w, r, "resources/index.html")
 }
