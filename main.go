4425a9df5e73c981fc0969989628fbc8424c773d
Peter Tinti <volgorean@gmail.com>
2014-10-25 05:15:40 -0700

    added file serve for css and js and rearanged structure

--- main.go
+++ main.go
@@ -6,9 +6,11 @@ import (
 
 func main() {
 	http.HandleFunc("/", rootController)
+	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("css"))))
+	http.Handle("/js/", http.StripPrefix("/js/", http.FileServer(http.Dir("js"))))
 	http.ListenAndServe(":8080", nil)
 }
 
 func rootController(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "main/index.html")
+	http.ServeFile(w, r, "index.html")
 }
